24264319737bc7ca9935a2c0f9f93fa469b55732
Seth Ullman <sethullman@Seths-MacBook-Pro.local>
2024-09-02 17:55:56 -0600

    add main class

---
+++ Main.py
@@ -0,0 +1,21 @@
+class Main:
+	def __init__(self, file):
+		dataFile = file
+		handler = new DataHandler()
+		trainingSet = None
+		noisyTrainingSet = None
+		testingSet = None
+		noisyTestingSet = None
+		testResults = None
+		cleanModel = new Model(trainingSet)
+		noisyModel = new Model(noisyTraingSet)
+
+	def train(self):
+		pass
+
+	def test(self):
+		pass
+
+	def visualizeResults(self):
+		pass
+
