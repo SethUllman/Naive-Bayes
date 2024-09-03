43ccd1a72a8810b7cf8f84205b8632616eeeb5d1
Seth Ullman <sethullman@Seths-MacBook-Pro.local>
2024-09-02 18:11:58 -0600

    add Datahandler class

---
+++ DataHandler.py
@@ -0,0 +1,13 @@
+class DataHandler:
+	def __init__(self, file):
+		datafile = file
+		workingData = None
+
+	def clean(self):
+		pass
+
+	def addNois(self):
+		pass
+
+	def separateSets(self):
+		pass
