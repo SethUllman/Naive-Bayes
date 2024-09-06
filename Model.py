3bd1d3db80b05d42e2f9c6495750c3f51295dd21
SethUllman <seth.ullman98@gmail.com>
2024-09-06 10:00:58 -0600

    add Model method explenations

--- Model.py
+++ Model.py
@@ -3,5 +3,18 @@ class Model:
 		self.trainingFolds = trainingFolds
 		self.testFold = testFold
 
+		self.classProbs = {}
+		self.conditionalProps = {}
+
+	#finds the probability of the classes possible values: step 1 in algorithm
+	def findClassProbs(self):
+		pass
+
+	#finds the probability of features given a class: main algorithm
+	def findConditionalProbs(self):
+		pass
+	
 	def train(self):
 		print("training model")
+		findClassProbs()
+		findConditionalProbs()
