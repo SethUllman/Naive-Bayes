bfbbfe16b323021382b8b054a4c68ddd06f435d4
Seth Ullman <sethullman@Seths-MacBook-Pro.local>
2024-09-07 08:52:40 -0600

    add find Class probabilities

--- Main.py
+++ Main.py
@@ -2,34 +2,35 @@ from DataHandler import DataHandler
 from Model import Model
 
 class Main:
-	def __init__(self, filePath, columnNames, className):
-		#initialize data handler, clean data, and separate into 10 folds
-		self.handler = DataHandler(filePath, columnNames)
-		self.cleanFolds = self.handler.separateSets(self.handler.workingData)
-		self.noisyFolds = self.handler.separateSets(self.handler.addNoise())
-		
-		#create 10 trained models for both the clean and noisy data
-		self.cleanModels = self.train(self.cleanFolds)
-		self.noisyModels = self.train(self.noisyFolds)
+    def __init__(self, filePath, columnNames, className):
+        #initialize data handler, clean data, and separate into 10 folds
+        self.handler = DataHandler(filePath, columnNames)
+        self.cleanFolds = self.handler.separateSets(self.handler.workingData)
+        self.noisyFolds = self.handler.separateSets(self.handler.addNoise())
+        self.className = className
+        
+        #create 10 trained models for both the clean and noisy data
+        self.cleanModels = self.train(self.cleanFolds)
+        self.noisyModels = self.train(self.noisyFolds)
 
 
-	#Uses the 10 folds passed in order to perform cross validation,
-	#creating 10 models which each use 1 of the folds as a test set
-	def train(self, folds):
-		models = []
+    #Uses the 10 folds passed in order to perform cross validation,
+    #creating 10 models which each use 1 of the folds as a test set
+    def train(self, folds):
+        models = []
 
-		for i in range(10):
-			model = Model(folds[0], folds[1:])
-			model.train()
-			models.append(model)
-			folds.append(folds[0])
-			folds.pop(0)
+        for i in range(10):
+            model = Model(folds[0], folds[1:], self.className)
+            model.train()
+            models.append(model)
+            folds.append(folds[0])
+            folds.pop(0)
 
-		return models
+        return models
 
-	def test(self):
-		pass
+    def test(self):
+        pass
 
-	def visualizeResults(self):
-		pass
+    def visualizeResults(self):
+        pass
 
--- Model.py
+++ Model.py
@@ -1,20 +1,41 @@
+import pandas as pd
+
 class Model:
-	def __init__(self, trainingFolds, testFold):
-		self.trainingFolds = trainingFolds
-		self.testFold = testFold
+    def __init__(self, testFold, trainingFolds, className):
+        self.trainingFolds = trainingFolds
+        self.testFold = testFold
+        self.className = className
 
-		self.classProbs = {}
-		self.conditionalProps = {}
+        self.classProbs = {}  #{Class1: prob, Class2: prob}
+        self.conditionalProps = {}  #{Class1: {Value1: Prob, Value2: Prob}, Class2: {}}
 
-	#finds the probability of the classes possible values: step 1 in algorithm
-	def findClassProbs(self):
-		pass
+    #finds the probability of the classes possible values: step 1 in algorithm
+    def findClassProbs(self):
+        valueCounts = []
+        totalLength = 0
 
-	#finds the probability of features given a class: main algorithm
-	def findConditionalProbs(self):
-		pass
-	
-	def train(self):
-		print("training model")
-		findClassProbs()
-		findConditionalProbs()
+        #for each fold, get a count of each class value as well as total length
+        for fold in self.trainingFolds:
+            count = fold[self.className].value_counts()
+            valueCounts.append(count)
+            totalLength += len(fold)
+
+        #sum class counts of all training folds, this creates a Series that
+        #we convert to a dictionary
+        totalCounts = pd.concat(valueCounts, axis=1).fillna(0).sum(axis=1)
+        countDict = dict(totalCounts)
+        countDict = {k: int(v) for k, v in countDict.items()}
+
+        #using our counts and total training entries, we find probabilities
+        #for all Class values
+        for key, value in countDict.items():
+            self.classProbs[key] = value / totalLength
+
+    #finds the probability of features given a class: main algorithm
+    def findConditionalProbs(self):
+        pass
+
+    def train(self):
+        print("training model")
+        self.findClassProbs()
+        self.findConditionalProbs()
